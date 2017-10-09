db0b897e1ac6c4ab1da9bf5d7d260eee2c223e2a
thumbd12856 <thumbd12856@gmail.com>
2017-10-09 10:16:51 +0800

    load all taipower data. find max value of all day

--- loadTaipowerData.py
+++ loadTaipowerData.py
@@ -25,27 +25,20 @@ for i in range(len(loadData)):
 	# cuase there is 2017-09-02 data in csv file, and the csv file is more accurate.
 	if loadData[i]["create_date"]["$date"][:10] == "2017-09-02": break;
 
-	# when loading data until to another day.
-	if tempDate != loadData[i]["create_date"]["$date"][:10]:
+	tempDate = loadData[i]["create_date"]["$date"][:10]
+	tempTime = loadData[i]["create_date"]["$date"][10:]
+	northSupply = float(loadData[i]["regionData"]["northSupply"])
+	northUsage = float(loadData[i]["regionData"]["northUsage"])
+	centerSupply = float(loadData[i]["regionData"]["centerSupply"])
+	centerUsage = float(loadData[i]["regionData"]["centerUsage"])
+	southSupply = float(loadData[i]["regionData"]["southSupply"])
+	southUsage = float(loadData[i]["regionData"]["southUsage"])
+	eastSupply = float(loadData[i]["regionData"]["eastSupply"])
+	eastUsage = float(loadData[i]["regionData"]["eastUsage"])
 
-		# append tuple data (day of power data) to insertData.
-		insertData.append(tuple((
-			tempDate, northSupply, northUsage, centerSupply, centerUsage, southSupply, southUsage, eastSupply, eastUsage
-		)))
-
-		tempDate = loadData[i]["create_date"]["$date"][:10]
-		northSupply = northUsage = centerSupply = centerUsage = southSupply = southUsage = eastSupply = eastUsage = 0.0
-
-	# add temp data to variables.
-	northSupply += float(loadData[i]["regionData"]["northSupply"])
-	northUsage += float(loadData[i]["regionData"]["northUsage"])
-	centerSupply += float(loadData[i]["regionData"]["centerSupply"])
-	centerUsage += float(loadData[i]["regionData"]["centerUsage"])
-	southSupply += float(loadData[i]["regionData"]["southSupply"])
-	southUsage += float(loadData[i]["regionData"]["southUsage"])
-	eastSupply += float(loadData[i]["regionData"]["eastSupply"])
-	eastUsage += float(loadData[i]["regionData"]["eastUsage"])
-
+	insertData.append(tuple((
+		tempDate, tempTime, northSupply, northUsage, centerSupply, centerUsage, southSupply, southUsage, eastSupply, eastUsage
+	)))
 
 # load Taipower data, csv file, from 2017/09/02 - 2017/09/23.
 for i in range(2, 24):
@@ -57,26 +50,23 @@ for i in range(2, 24):
 	file = "./data/Taipower/" + date + ".csv"
 	f = open(file, 'r')
 
-	# add temp data to variables.
-	northSupply = northUsage = centerSupply = centerUsage = southSupply = southUsage = eastSupply = eastUsage = 0.0
 	for row in csv.reader(f):
-		northSupply += float(row[1])
-		northUsage += float(row[2])
-		centerSupply += float(row[3])
-		centerUsage += float(row[4])
-		southSupply += float(row[5])
-		southUsage += float(row[6])
-		eastSupply += float(row[7])
-		eastUsage += float(row[8])
+		northSupply = float(row[1])
+		northUsage = float(row[2])
+		centerSupply = float(row[3])
+		centerUsage = float(row[4])
+		southSupply = float(row[5])
+		southUsage = float(row[6])
+		eastSupply = float(row[7])
+		eastUsage = float(row[8])
+
+		insertData.append(tuple((
+			date, row[0], northSupply, northUsage, centerSupply, centerUsage, southSupply, southUsage, eastSupply, eastUsage
+		)))
 	f.close()
 
-	# append tuple data (day of power data) to insertData.
-	insertData.append(tuple((
-		date, northSupply, northUsage, centerSupply, centerUsage, southSupply, southUsage, eastSupply, eastUsage
-	)))
-	
 # insert data to database.
-insert = ("INSERT INTO `test` (date, northSupply, northUsage, centerSupply, centerUsage, southSupply, southUsage, eastSupply, eastUsage) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
+insert = ("INSERT INTO `test` (date, time, northSupply, northUsage, centerSupply, centerUsage, southSupply, southUsage, eastSupply, eastUsage) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
 cursor.executemany(insert, insertData);
 connet.commit()
 cursor.close()
